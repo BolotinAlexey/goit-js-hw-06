961a8287beb1d92b6c8b8e01ee5930d50f732554
Oleksii Bolotin <samkiral73@gmail.com>
2023-01-12 12:21:55 +0100

    finish task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
-
+const formEl = document.querySelector('.login-form'); // form.login-form
+const onSubmitForm = e => {
+  e.preventDefault();
+  const { email, password } = e.currentTarget.elements;
+  if (!(email.value && password.value)) {
+    email.classList.add(email.value ? 'valid' : 'invalid');
+    password.classList.add(password.value ? 'valid' : 'invalid');
+    setTimeout(() => {
+      alert('input data');
+    });
+  } else {
+    const obj = { email: email.value, password: password.value };
+    console.log(obj);
+    formEl.reset();
+  }
+};
+formEl.addEventListener('submit', onSubmitForm);
