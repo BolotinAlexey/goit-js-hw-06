094a0fb70041be329541dba24150ef2b657041c1
Oleksii Bolotin <samkiral73@gmail.com>
2023-01-12 13:15:01 +0100

    fix bug classlist task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,10 +1,14 @@
 const formEl = document.querySelector('.login-form'); // form.login-form
+const resetStyles = () => {};
 const onSubmitForm = e => {
   e.preventDefault();
   const { email, password } = e.currentTarget.elements;
+  email.className = password.className = '';
+
   if (!(email.value && password.value)) {
     email.classList.add(email.value ? 'valid' : 'invalid');
     password.classList.add(password.value ? 'valid' : 'invalid');
+
     setTimeout(() => {
       alert('input data');
     });
@@ -15,3 +19,16 @@ const onSubmitForm = e => {
   }
 };
 formEl.addEventListener('submit', onSubmitForm);
+
+//---------------OR-------------
+
+// const formEl = document.querySelector('.login-form'); // form.login-form
+// formEl.addEventListener('submit', e => {
+//   e.preventDefault();
+//   const { email, password } = e.currentTarget.elements;
+//   if (!(email.value && password.value)) alert('input data');
+//   else {
+//     console.log({ email: email.value, password: password.value });
+//     formEl.reset();
+//   }
+// });
